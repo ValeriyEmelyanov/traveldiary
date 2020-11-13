7a5644ae9529dc09df75b7eca5a1844f1874a1aa
Valery Emelyanov <emelva@rambler.ru>
2020-11-13 14:27:06 +0300

    Refactoring DateTimeService

--- src/main/java/com/example/traveldiary/service/DateTimeService.java
+++ src/main/java/com/example/traveldiary/service/DateTimeService.java
@@ -1,35 +1,6 @@
 package com.example.traveldiary.service;
 
-import org.springframework.stereotype.Service;
-
-import java.time.LocalDateTime;
-import java.time.LocalTime;
-import java.time.format.DateTimeFormatter;
-
-@Service
-public class DateTimeService {
-    private static final LocalTime SIX_PM = LocalTime.of(18, 0);
-
-    public String getNow() {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
-        return String.format("Now is %s", formatter.format(LocalDateTime.now()));
-    }
-
-    public String getGreeting(String name) {
-        return String.format("%s%s!", defineGreeting(), name == null ? "" : ", " + name);
-    }
-
-    private String defineGreeting() {
-        LocalTime time = LocalTime.now();
-
-        if (time.isAfter(LocalTime.MIDNIGHT) && time.isBefore(LocalTime.NOON)) {
-            return "Good morning";
-        }
-
-        if (time.equals(LocalTime.NOON) || (time.isAfter(LocalTime.NOON) && time.isBefore(SIX_PM))) {
-            return "Good afternoon";
-        }
-
-        return "Good evening";
-    }
+public interface DateTimeService {
+    String getNow();
+    String getGreeting(String name);
 }
---
+++ src/main/java/com/example/traveldiary/service/impl/DateTimeServiceImpl.java
@@ -0,0 +1,39 @@
+package com.example.traveldiary.service.impl;
+
+import com.example.traveldiary.service.DateTimeService;
+import org.springframework.stereotype.Service;
+
+import javax.persistence.OneToOne;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
+@Service
+public class DateTimeServiceImpl implements DateTimeService {
+    private static final LocalTime SIX_PM = LocalTime.of(18, 0);
+
+    @Override
+    public String getNow() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+        return String.format("Now is %s", formatter.format(LocalDateTime.now()));
+    }
+
+    @Override
+    public String getGreeting(String name) {
+        return String.format("%s%s!", defineGreeting(), name == null ? "" : ", " + name);
+    }
+
+    private String defineGreeting() {
+        LocalTime time = LocalTime.now();
+
+        if (time.isAfter(LocalTime.MIDNIGHT) && time.isBefore(LocalTime.NOON)) {
+            return "Good morning";
+        }
+
+        if (time.equals(LocalTime.NOON) || (time.isAfter(LocalTime.NOON) && time.isBefore(SIX_PM))) {
+            return "Good afternoon";
+        }
+
+        return "Good evening";
+    }
+}
